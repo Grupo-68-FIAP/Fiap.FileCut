72b32dcafc928d25753bee52c28755d101812e4c
ervinnotari <ervinnotari@hotmail.com>
2025-01-12 02:37:09 -0300

    Add cors configurations

---
+++ Fiap.FileCut.Infra.Api/Configurations/CorsConfig.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fiap.FileCut.Infra.Api.Configurations
+{
+    public static class CorsConfig
+    {
+        public static void AddEnvCors(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddCors(options =>
+            {
+                var allowOrigins = configuration.GetValue<string>("ALLOW_ORIGINS") ?? "*";
+                options.AddPolicy("CorsConfig", builder => builder.ConfigureCorsPolicy(allowOrigins));
+            });
+        }
+
+        private static void ConfigureCorsPolicy(this CorsPolicyBuilder builder, string allowOrigins)
+        {
+            if (allowOrigins == "*")
+            {
+                builder.AllowAnyOrigin();
+            } else
+            {
+                builder.WithOrigins(allowOrigins.Split(';'));
+            }
+            builder.AllowAnyMethod().AllowAnyHeader();
+        }
+
+        public static void UseEnvCors(this IApplicationBuilder app)
+        {
+            app.UseCors("CorsPolicy");
+        }
+    }
+}
