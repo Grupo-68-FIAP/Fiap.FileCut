4bffe6ec746bc57f6d80789ba340f97ad12c9327
ervinnotari <ervinnotari@hotmail.com>
2025-01-19 22:25:29 -0300

    add

---
+++ Fiap.FileCut.Core/Objects/SmtpProperties.cs
@@ -0,0 +1,10 @@
+﻿namespace Fiap.FileCut.Core.Objects
+{
+    public class SmtpProperties(string server, int port, string username, string password)
+    {
+        public string Server { get; set; } = server;
+        public int Port { get; set; } = port;
+        public string Username { get; set; } = username;
+        public string Password { get; set; } = password;
+    }
+}
---
+++ Fiap.FileCut.Infra.Api/Configurations/SmtpConfig.cs
@@ -0,0 +1,45 @@
+﻿using Fiap.FileCut.Core.Objects;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fiap.FileCut.Infra.Api.Configurations
+{
+    public static class SmtpConfig
+    {
+        public static void AddSmtpConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+            var config = GetSmatpConfig(configuration);
+            if (config != null)
+                services.ConfigureOptions(config);
+        }
+
+        internal static SmtpProperties? GetSmatpConfig(IConfiguration configuration)
+        {
+            var smtpServer = Environment.GetEnvironmentVariable("SMTP_SERVER");
+            var smtpPort = Environment.GetEnvironmentVariable("SMTP_PORT");
+            var smtpUsername = Environment.GetEnvironmentVariable("SMTP_USERNAME");
+            var smtpPassword = Environment.GetEnvironmentVariable("SMTP_PASSWORD");
+
+            if (!string.IsNullOrEmpty(smtpServer) || !string.IsNullOrEmpty(smtpPort) || !string.IsNullOrEmpty(smtpUsername) || !string.IsNullOrEmpty(smtpPassword))
+            {
+                if (smtpServer == null)
+                    throw new MissingFieldException("To configure the SMTP server, you must provide the SMTP_SERVER environment variable.");
+                if (smtpPort == null)
+                    throw new MissingFieldException("To configure the SMTP port, you must provide the SMTP_PORT environment variable.");
+                if (smtpUsername == null)
+                    throw new MissingFieldException("To configure the SMTP username, you must provide the SMTP_USERNAME environment variable.");
+                if (smtpPassword == null)
+                    throw new MissingFieldException("To configure the SMTP password, you must provide the SMTP_PASSWORD environment variable.");
+
+                return new SmtpProperties(smtpServer, int.Parse(smtpPort), smtpUsername, smtpPassword);
+            }
+
+            var smtpConfigurationSection = configuration.GetSection("SmtpConfig");
+            var confg = smtpConfigurationSection.Get<SmtpProperties>();
+            if (confg != null)
+                return confg;
+
+            return null;
+        }
+    }
+}
