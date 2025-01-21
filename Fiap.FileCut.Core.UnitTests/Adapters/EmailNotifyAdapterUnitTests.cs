67b7b60a6fbfc5cc771f6f9125e25e22d4d60a5a
ervinnotari <ervinnotari@hotmail.com>
2025-01-20 23:24:48 -0300

    Add sonar coverage

--- Fiap.FileCut.Core.UnitTests/Adapters/EmailNotifyAdapterUnitTests.cs
+++ Fiap.FileCut.Core.UnitTests/Adapters/EmailNotifyAdapterUnitTests.cs
@@ -1,4 +1,5 @@
-﻿using Fiap.FileCut.Core.Adapters;
+﻿using Castle.Core.Logging;
+using Fiap.FileCut.Core.Adapters;
 using Fiap.FileCut.Core.Interfaces.Factories;
 using Fiap.FileCut.Core.Objects;
 using Microsoft.Extensions.Logging;
@@ -24,6 +25,71 @@ namespace Fiap.FileCut.Core.UnitTests.Adapters
             adapter.NotifyAsync(notifyContext);
             // Assert
             smtpClient.Verify(x => x.SendMailAsync(mailMessage), Times.Once);
+        }
+
+        [Fact]
+        public void NotifyAsync_WhenCalledWithFileCutMailMessage_ShouldCallSendFileCutMailMessageAsync()
+        {
+            // Arrange
+            var logger = new Mock<ILogger<EmailNotifyAdapter>>();
+            logger.Setup(x => x.Log(It.IsAny<LogLevel>(), It.IsAny<EventId>(), It.IsAny<It.IsAnyType>(),
+                It.IsAny<Exception?>(), It.IsAny<Func<It.IsAnyType, Exception?, string>>()))
+                .Verifiable();
+            var smtpClient = new Mock<ISmtpClient>();
+            smtpClient.Setup(x => x.SendMailAsync(It.IsAny<MailMessage>())).Returns(Task.CompletedTask);
+            var adapter = new EmailNotifyAdapter(logger.Object, smtpClient.Object);
+            var fileCutMailMessage = new FileCutMailMessage("test@test.com");
+            var notifyContext = new NotifyContext<FileCutMailMessage>(fileCutMailMessage, Guid.NewGuid());
+            // Act
+            adapter.NotifyAsync(notifyContext);
+            // Assert
+            smtpClient.Verify(x => x.SendMailAsync(It.IsAny<MailMessage>()), Times.Once);
+            logger.Verify(x => x.Log(
+                It.Is<LogLevel>(ll => ll == LogLevel.Debug),
+                It.IsAny<EventId>(),
+                It.IsAny<It.IsAnyType>(),
+                It.IsAny<Exception?>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()), Times.Once);
+        }
+
+        [Fact]
+        public void NotifyAsync_WhenCalledWithInvalidContext_ShouldNotCallSendMailMessageAsync()
+        {
+            // Arrange
+            var logger = new Mock<ILogger<EmailNotifyAdapter>>();
+            var smtpClient = new Mock<ISmtpClient>();
+            smtpClient.Setup(x => x.SendMailAsync(It.IsAny<MailMessage>())).Returns(Task.CompletedTask);
+            var adapter = new EmailNotifyAdapter(logger.Object, smtpClient.Object);
+            var notifyContext = new NotifyContext<string>("test", Guid.NewGuid());
+            // Act
+            adapter.NotifyAsync(notifyContext);
+            // Assert
+            smtpClient.Verify(x => x.SendMailAsync(It.IsAny<MailMessage>()), Times.Never);
+        }
+
+        [Fact]
+        public void SendEmailAsync_WhenCalledWithSmtpClientInvalid_ShouldLogError()
+        {
+            // Arrange
+            var logger = new Mock<ILogger<EmailNotifyAdapter>>();
+            logger.Setup(x => x.Log(It.IsAny<LogLevel>(), It.IsAny<EventId>(), It.IsAny<It.IsAnyType>(),
+                It.IsAny<Exception?>(), It.IsAny<Func<It.IsAnyType, Exception?, string>>()))
+                .Verifiable();
+            var smtpClient = new Mock<ISmtpClient>();
+            smtpClient.Setup(x => x.SendMailAsync(It.IsAny<MailMessage>())).Throws(new Exception());
+            var adapter = new EmailNotifyAdapter(logger.Object, smtpClient.Object);
+            var userId = Guid.NewGuid();
+            var fileCutMailMessage = new FileCutMailMessage("test@test.com");
+            var notifyContext = new NotifyContext<FileCutMailMessage>(fileCutMailMessage, userId);
+            // Act
+            adapter.NotifyAsync(notifyContext);
+            // Assert
+            logger.Verify(x => x.Log(
+                It.Is<LogLevel>(ll => ll == LogLevel.Error), 
+                It.IsAny<EventId>(),
+                It.IsAny<It.IsAnyType>(),
+                It.IsAny<Exception?>(),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()), Times.Once);
         }
     }
 }
