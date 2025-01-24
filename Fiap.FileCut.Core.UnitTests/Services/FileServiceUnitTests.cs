94b2e9cdd218a5c768c0287cb6ba08493305c55c
ervinnotari <ervinnotari@hotmail.com>
2025-01-23 21:17:39 -0300

    test

---
+++ Fiap.FileCut.Core.UnitTests/Services/FileServiceUnitTests.cs
@@ -0,0 +1,26 @@
+﻿using Fiap.FileCut.Core.Interfaces.Repository;
+using Fiap.FileCut.Core.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.FileCut.Core.UnitTests.Services;
+
+public class FileServiceUnitTests
+{
+    [Fact]
+    public async Task GetAllFilesName_WhenCalled_ShouldReturnAllFilesName()
+    {
+        // Arrange
+        var userId = Guid.NewGuid();
+        var fileRepositoryMock = new Mock<IFileRepository>();
+        var fileService = new FileService(fileRepositoryMock.Object);
+        // Act
+        _ = await Assert.ThrowsAsync<NotImplementedException>(async () => await fileService.GetAllFilesName(userId));
+    }
+}
