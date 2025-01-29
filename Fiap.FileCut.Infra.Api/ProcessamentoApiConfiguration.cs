5973fae30e475fbdd5cd4d62c0ba85a3bf823aee
ervinnotari <ervinnotari@hotmail.com>
2025-01-29 20:15:59 -0300

    mudado as configurações a api de processamento para a lib de infra.api e adicionado arquivo docker-compose

---
+++ Fiap.FileCut.Infra.Api/ProcessamentoApiConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Fiap.FileCut.Infra.Api.Configurations;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Fiap.FileCut.Infra.Api
+{
+    public static class ProcessamentoApiConfiguration
+    {
+        public static Task ConfigureFileCutProcessamentoApi(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddJwtBearerAuthentication();
+            builder.Services.AddSwaggerFC();
+            builder.Services.AddEnvCors();
+
+            return Task.CompletedTask;
+        }
+
+        public static Task ScopedFileCutProcessamentoApi(this IServiceScope scope)
+        {
+            return Task.CompletedTask;
+        }
+
+        public static Task InitializeFileCutProcessamentoApi(this IApplicationBuilder app)
+        {
+            app.UseSwaggerFC();
+            app.UseEnvCors();
+            app.UseHttpsRedirection();
+            app.UseAuth();
+            return Task.CompletedTask;
+        }
+    }
+}
