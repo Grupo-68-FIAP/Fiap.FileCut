bbe3fe91e0af20bf77778da8958e3f3a574e2dd6
Felipe Tomm <felipe.tomm@gmail.com>
2025-02-06 02:12:31 -0300

    Ajustes testes unitários

--- Fiap.FileCut.Core.UnitTests/Adapters/UserNotifyQueuePublishTests.cs
+++ Fiap.FileCut.Core.UnitTests/Adapters/UserNotifyQueuePublishTests.cs
@@ -1,27 +1,32 @@
 ﻿using Fiap.FileCut.Core.Adapters;
 using Fiap.FileCut.Core.Interfaces.Services;
-using Fiap.FileCut.Core.Objects;
 using Fiap.FileCut.Core.Objects.QueueEvents;
+using Fiap.FileCut.Core.Objects;
 using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Fiap.FileCut.Core.UnitTests.Adapters;
 
-public class VideoUploadedQueuePublishTests
+public class UserNotifyQueuePublishTests
 {
     [Fact]
     public async Task NotifyAsync_Should_Call_PublishAsync_When_Correct_Type()
     {
         // Arrange
         var mockService = new Mock<IMessagingPublisherService>();
-        var queuePublish = new VideoUploadedQueuePublish(mockService.Object);
+        var queuePublish = new UserNotifyQueuePublish(mockService.Object);
         var userId = Guid.NewGuid();
-        var notifyContext = new NotifyContext<VideoUploadedEvent>(new VideoUploadedEvent("VideoTest"), userId);
+        var notifyContext = new NotifyContext<UserNotifyEvent>(new UserNotifyEvent("VideoTest"), userId);
 
         // Act
         await queuePublish.NotifyAsync(notifyContext);
 
         // Assert
-        mockService.Verify(s => s.PublishMessage(It.IsAny<NotifyContext<VideoUploadedEvent>>()), Times.Once);
+        mockService.Verify(s => s.PublishMessage(It.IsAny<NotifyContext<UserNotifyEvent>>()), Times.Once);
     }
 
     [Fact]
@@ -29,9 +34,9 @@ public class VideoUploadedQueuePublishTests
     {
         // Arrange
         var mockService = new Mock<IMessagingPublisherService>();
-        var queuePublish = new VideoUploadedQueuePublish(mockService.Object);
+        var queuePublish = new UserNotifyQueuePublish(mockService.Object);
         var userId = Guid.NewGuid();
-        var notifyContext = new NotifyContext<VideoUploadedEvent>(new VideoUploadedEvent("VideoTest"), userId);
+        var notifyContext = new NotifyContext<UserNotifyEvent>(new UserNotifyEvent("VideoTest"), userId);
 
         // Act
         await queuePublish.PublishAsync(notifyContext);
